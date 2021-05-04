25b778910b04f0ffaa3117543969fb09ee4e8afb
Stanley Umezuruike <manstylo@gmail.com>
2021-05-04 18:20:59 +0100

    First Commit

---
+++ app/src/main/java/com/codehunta/funfact/DummyData.kt
@@ -0,0 +1,31 @@
+package com.codehunta.funfact
+
+object DummyData {
+    val funFacts = listOf<FactModel>(
+        FactModel(
+            name = "Kotlin",
+            R.drawable.kt_logo,
+            fact = "ipso facto abi na lorem whatever text. You get the jerk"
+        ),
+        FactModel(
+            name = "Java",
+            R.drawable.java_logo,
+            fact = "ipso facto abi na lorem whatever text. You get the jerk"
+        ),
+        FactModel(
+            name = "Python",
+            R.drawable.py_logo,
+            fact = "ipso facto abi na lorem whatever text. You get the jerk"
+        ),
+        FactModel(
+            name = "JavaScipt",
+            R.drawable.js_logo,
+            fact = "ipso facto abi na lorem whatever text. You get the jerk"
+        ),
+        FactModel(
+            name = "PHP",
+            R.drawable.php_logo,
+            fact = "ipso facto abi na lorem whatever text. You get the jerk"
+        )
+    )
+}
---
+++ app/src/main/java/com/codehunta/funfact/FactModel.kt
@@ -0,0 +1,3 @@
+package com.codehunta.funfact
+
+data class FactModel(val name: String, val logo: Int, val fact: String)
