037cf5f07a5715a63260b50eca88dfec75d8d70a
Stanley Umezuruike <manstylo@gmail.com>
2021-05-10 00:18:47 +0100

    Login Details

--- app/src/main/java/com/codehunta/funfact/LoginActivity.kt
+++ app/src/main/java/com/codehunta/funfact/LoginActivity.kt
@@ -1,11 +1,37 @@
 package com.codehunta.funfact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.codehunta.funfact.databinding.ActivityLoginBinding
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private var binding:ActivityLoginBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding?.root)
+
+        val nextButton = findViewById<MaterialButton>(R.id.btn_next)
+        nextButton.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val email = binding?.inputEmail.toString()
+        val password = binding?.inputPassword.toString()
+
+        if (email == "codehunta@zuri.com" && password == "SuperM4n") {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }else {
+            Toast.makeText(this, "Provide appropriate email & password to login", Toast.LENGTH_LONG).show()
+        }
     }
 }
