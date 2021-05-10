f4ee764ceaa1eae2d0d5020250bc48224afea748
Stanley Umezuruike <manstylo@gmail.com>
2021-05-10 13:09:47 +0100

    It works!

--- app/src/main/java/com/codehunta/funfact/LoginActivity.kt
+++ app/src/main/java/com/codehunta/funfact/LoginActivity.kt
@@ -10,22 +10,22 @@ import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
 
-    private var binding:ActivityLoginBinding? = null
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding?.root)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val nextButton = findViewById<MaterialButton>(R.id.btn_next)
+        val nextButton = findViewById<MaterialButton>(R.id.btn_login)
         nextButton.setOnClickListener {
-            login()
+            userLogin()
         }
     }
 
-    private fun login() {
-        val email = binding?.inputEmail.toString()
-        val password = binding?.inputPassword.toString()
+    private fun userLogin() {
+        val email = binding.inputEmail.text.toString()
+        val password = binding.inputPassword.text.toString()
 
         if (email == "codehunta@zuri.com" && password == "SuperM4n") {
             val intent = Intent(this, MainActivity::class.java)
