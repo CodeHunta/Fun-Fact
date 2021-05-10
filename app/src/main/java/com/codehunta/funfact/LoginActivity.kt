d4042c10adda389cf08ee02bc3628f917946487e
Stanley Umezuruike <manstylo@gmail.com>
2021-05-10 13:18:40 +0100

    Safe Call!

--- app/src/main/java/com/codehunta/funfact/LoginActivity.kt
+++ app/src/main/java/com/codehunta/funfact/LoginActivity.kt
@@ -10,12 +10,12 @@ import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private var binding: ActivityLoginBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
         val nextButton = findViewById<MaterialButton>(R.id.btn_login)
         nextButton.setOnClickListener {
@@ -24,8 +24,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun userLogin() {
-        val email = binding.inputEmail.text.toString()
-        val password = binding.inputPassword.text.toString()
+        val email = binding?.inputEmail?.text.toString()
+        val password = binding?.inputPassword?.text.toString()
 
         if (email == "codehunta@zuri.com" && password == "SuperM4n") {
             val intent = Intent(this, MainActivity::class.java)
